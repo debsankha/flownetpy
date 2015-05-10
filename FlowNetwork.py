25c1c2f9080374adc905dced7fcdb2a5029fb053
debsankha manik <dmanik@nld.ds.mpg.de>
2015-05-10 23:07:36 +0200

    Renamed dynamics to FlowModel

--- FlowNetwork.py
+++ FlowNetwork.py
@@ -1,21 +1,29 @@
 import networkx as nx
 
 class FlowNetwork(nx.Graph):
-	def __init__(self,graph, input):
-	"""
-	Creates a flow network with network topology given by `graph` and the input at each node given by `input`
-	
-	Arguments:
-		`graph`: Any valid networkx graph
-		`input`: A dictionary with nodes of `graph` as keys
+    def __init__(self,graph, input, flowmodel):
+        """
+        Creates a flow network with network topology given by `graph` and the input at each node given by `input`
+        
+        Arguments:
+            `graph`: Any valid networkx graph
+            `input`: A dictionary with nodes of `graph` as keys"""
+        
+        nx.Graph.__init__(self,graph)
+        
+        self.flowmodel=flowmodel
 
-	"""
-		nx.Graph.__init__(self,graph)
+        for node in input.keys():
+            self.node[node]['input']=input[node]
 
-		#making sure all the nodes have a defined input value
-		assert(self.nodes()==input.keys())
 
-		for node in input.keys():
-			self.node[node]['input']=input[node]
-	
-	
+    def fixed_point(self, **kwargs):
+        """
+        Returns a set of steady state flows in the network, in form of a dictionary, with each edge as a key. With additional parameters that lead to this fixed point, as well. 
+        """
+        return self.flowmodel.fixed_point(self, **kwargs)
+
+
+
+        
+             
---
+++ flowmodels/linear.py
@@ -0,0 +1,3 @@
+def fixed_point(flownet):
+    
+
