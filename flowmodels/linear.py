5c0aecee9ea438f8d6b4d3bfe07a3c01873b7032
Debsankha Manik <dmanik@nld.ds.mpg.de>
2015-08-21 20:00:37 +0200

    Added unittests

--- flowmodels/linear.py
+++ flowmodels/linear.py
@@ -2,22 +2,20 @@ import numpy as np
 import networkx as nx
 
 
-#The linear Poiseullie flow in a network
+# The linear Poiseullie flow in a network
 
 def fixed_point(flownet):
     """
     The fixed points are given by:
         \sum_j (p_j-p_i)
     """
-    L=np.array(nx.laplacian_matrix(flownet))
-    I=np.array([flownet.node[n]['input'] for n in flownet.nodes()])
+    L = np.array(nx.laplacian_matrix(flownet))
+    I = np.array([flownet.node[n]['input'] for n in flownet.nodes()])
 
-    pressures=np.insert(np.dot(np.linalg.inv(L[1:,1:]), I[1:]), 0, 0)
+    pressures = np.insert(np.dot(np.linalg.inv(L[1:, 1:]), I[1:]), 0, 0)
 
-    node_indices={node:idx for idx,node in enumerate(flownet.nodes())}
+    node_indices = {node: idx for idx, node in enumerate(flownet.nodes())}
 
-    if flownet.weight_attr:
-        flows={(u,v):(pressures[node_indices[u]]-pressures[node_indices[v]])*dat[flownet.weight_attr] for (u,v,dat) in flownet.edges(data=True)}
-    else:
-        flows={(u,v):pressures[node_indices[u]]-pressures[node_indices[v]] for (u,v) in flownet.edges()}
+    flows = {(u, v): (pressures[node_indices[u]] - pressures[node_indices[v]]) * dat.get(
+        flownet.weight_attr, 1) for (u, v, dat) in flownet.edges(data=True)}
     return flows
---
+++ flowmodels/tests/test_linear.py
@@ -0,0 +1,22 @@
+from hypothesis import given, assume
+import hypothesis.strategies as st
+
+
+from nose.tools import *
+
+from flownetwork import FlowNetwork
+from flowmodels import linear
+
+import numpy as np
+import networkx as nx
+
+
+class TestFixedpoint:
+    
+    def setUp(self):
+        ring_graph = nx.cycle_graph(8)
+        inputs = {node: 1 for node in ring_graph.nodes()}
+        self.ring_net = FlowNetwork(ring_graph, inputs, linear)
+
+    
+    def test_
