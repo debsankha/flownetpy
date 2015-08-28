f93ae4f84ae4e06a6bd799ec82302ac07f61b6be
Debsankha Manik <deb5890@gmail.com>
2015-08-28 02:34:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.2',
 
     description='A package to simulate flow networks',
     long_description=long_description,
